taspelund/frr

zebra: enable arp-reply/NA snooping on br members for fast-failover in MH
If multihoming is enabled with fast-failover we need need to redirect
unicast ARP-replies and Neighbor-Advertisements in user space if the
DMAC points to an operationally down ES.

If fast failover is enabled we expect the dataplane (ASIC) to redirect
traffic headed to the down-ES via the VxLAN overlay. However ARP packets
on bridge access members are not forwarded by the ASIC. They are sent to
the CPU to be forwarded by the kernel post ARP suppression decisions.
Currently the kernel does not handle fast-failover (traffic redirection).
So we need to workaround that limitation in user space.

We do the user-space redirection currently in zebra as it has all the
necessary databases (VLAN->VNI mapping, MAC/FDB and ES tables). A raw
socket is created for every bridge-access port if MH is enabled (on
addition of first local ES). This socket listens on ARP-replies and
NAs. A read thread is also created per-bridge-access-port to read and
redirect these packets via the VxLAN overlay (vxlan encapsulates and
sends) if needed.

Use case example:
1. H11 and H12 are multi-homed to L11 and L12.
2. Say link between L12<=>H12 is down.
3. H12 sends a ARP request to H11.
4. H11 replies via the link attached to L12.
5. L12 rxes the packet and finds the DMAC (H12) via an ES that is
oper-down.
6. L12 (zebra) redirects the packet via (the VxLAN overlay) L11 to H12.

Sample output:
>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
torm-11# sh evpn arp-nd-redirect
EVPN ARP-reply/NA redirect: enabled
Stats:
  IPv4 ARP replies: 11
  IPv6 neighbor advertisements: 538
  Redirected packets: 9
  Skipped packets:    Not ready: 0
    VNI missing: 0
    MAC missing: 25
    Dest is not local ES: 513
    Dest ES oper-up: 2
torm-11#
>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>

Ticket: CM-30135

Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

zebra/zebra_vty.c
@@ -55,6 +55,7 @@
 #include "zebra/zebra_pbr.h"
 #include "zebra/zebra_nhg.h"
 #include "zebra/zebra_evpn_mh.h"
+#include "zebra/zebra_evpn_arp_nd.h"
 #include "zebra/interface.h"
 
 extern int allow_delete;
@@ -2450,6 +2451,20 @@ DEFUN (show_evpn_global,
 	return CMD_SUCCESS;
 }
 
+DEFUN (show_evpn_arp_redirect,
+       show_evpn_arp_nd_redirect_cmd,
+       "show evpn arp-nd-redirect[json]",
+       SHOW_STR
+       "EVPN\n"
+       "ARP/NA packet redirect\n"
+       JSON_STR)
+{
+	bool uj = use_json(argc, argv);
+
+	zebra_evpn_arp_nd_print_summary(vty, uj);
+	return CMD_SUCCESS;
+}
+
 DEFPY(show_evpn_l2_nh,
       show_evpn_l2_nh_cmd,
       "show evpn l2-nh [json$json]",
@@ -3727,6 +3742,7 @@ void zebra_vty_init(void)
 
 	install_element(VIEW_NODE, &show_frr_cmd);
 	install_element(VIEW_NODE, &show_evpn_global_cmd);
+	install_element(VIEW_NODE, &show_evpn_arp_nd_redirect_cmd);
 	install_element(VIEW_NODE, &show_evpn_vni_cmd);
 	install_element(VIEW_NODE, &show_evpn_vni_detail_cmd);
 	install_element(VIEW_NODE, &show_evpn_vni_vni_cmd);

ashred-lnx/ashred-lnx

(cherry picked from commit 32a11aa6b1009d640d4153dff680c3147d19bdff)

zebra/zebra_vty.c
@@ -44,6 +44,7 @@
 #include "zebra/zebra_pbr.h"
 #include "zebra/zebra_nhg.h"
 #include "zebra/zebra_evpn_mh.h"
+#include "zebra/zebra_evpn_arp_nd.h"
 #include "zebra/interface.h"
 #include "northbound_cli.h"
 #include "zebra/zebra_nb.h"
@@ -2954,6 +2955,17 @@ DEFPY(show_neigh,
 {
 	zebra_neigh_show(vty, AF_UNSPEC, !!json);
 
+DEFPY (show_evpn_arp_redirect,
+       show_evpn_arp_nd_redirect_cmd,
+       "show evpn arp-nd-redirect[json]",
+       SHOW_STR
+       "EVPN\n"
+       "ARP/NA packet redirect\n"
+       JSON_STR)
+{
+	bool uj = use_json(argc, argv);
+
+	zebra_evpn_arp_nd_print_summary(vty, uj);
 	return CMD_SUCCESS;
 }
 
@@ -4514,6 +4526,7 @@ void zebra_vty_init(void)
 
 	install_element(VIEW_NODE, &show_frr_cmd);
 	install_element(VIEW_NODE, &show_evpn_global_cmd);
+	install_element(VIEW_NODE, &show_evpn_arp_nd_redirect_cmd);
 	install_element(VIEW_NODE, &show_evpn_vni_cmd);
 	install_element(VIEW_NODE, &show_evpn_vni_detail_cmd);
 	install_element(VIEW_NODE, &show_evpn_vni_vni_cmd);